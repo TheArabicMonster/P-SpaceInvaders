6c53f2a5653cbc4e3b3cad2533e4e398025491cf
TheArabicMonster <102302877+TheArabicMonster@users.noreply.github.com>
2023-10-06 10:18:58 +0200

    main

---
+++ Code/SpicyConso/Display/Menu.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Display
+{
+    public class Menu
+    {
+        public string[] MenuTitre =
+        {
+            @" _____ ______   ___   _____  _____   _____  _   _  _   _   ___  ______  _____ ______ ",
+            @"/  ___|| ___ \ / _ \ /  __ \|  ___| |_   _|| \ | || | | | / _ \ |  _  \|  ___|| ___ \",
+            @"\ `--. | |_/ // /_\ \| /  \/| |__     | |  |  \| || | | |/ /_\ \| | | || |__  | |_/ /",
+            @" `--. \|  __/ |  _  || |    |  __|    | |  | . ` || | | ||  _  || | | ||  __| |    / ",
+            @"/\__/ /| |    | | | || \__/\| |___   _| |_ | |\  |\ \_/ /| | | || |/ / | |___ | |\ \ ",
+            @"\____/ \_|    \_| |_/ \____/\____/   \___/ \_| \_/ \___/ \_| |_/|___/  \____/ \_| \_|",
+        };
+        public string[] MenuJouer =
+        {
+            @"   ___  _____  _   _  _____ ______ ",
+            @"  |_  ||  _  || | | ||  ___|| ___ \",
+            @"    | || | | || | | || |__  | |_/ /",
+            @"    | || | | || | | ||  __| |    / ",
+            @"/\__/ /\ \_/ /| |_| || |___ | |\ \ ",
+            @"\____/  \___/  \___/ \____/ \_| \_|",
+        };
+        public string[] MenuOption =
+        {
+            @" _____ ______  _____  _____  _____  _   _ ",
+            @"|  _  || ___ \|_   _||_   _||  _  || \ | |",
+            @"| | | || |_/ /  | |    | |  | | | ||  \| |",
+            @"| | | ||  __/   | |    | |  | | | || . ` |",
+            @"\ \_/ /| |      | |   _| |_ \ \_/ /| |\  |",
+            @" \___/ \_|      \_/   \___/  \___/ \_| \_/"
+        };
+        public string[] MenuScore =
+        {
+            @" _____  _____  _____ ______  _____ ",
+            @"/  ___|/  __ \|  _  || ___ \|  ___|",
+            @"\ `--. | /  \/| | | || |_/ /| |__  ",
+            @" `--. \| |    | | | ||    / |  __| ",
+            @"/\__/ /| \__/\\ \_/ /| |\ \ | |___ ",
+            @"\____/  \____/ \___/ \_| \_|\____/ ",
+        };
+        public int MenuCount = 0;
+        public Menu()
+        {
+            this.MenuCount = 0;
+        }
+        public void DessinerMenuTitre()
+        {
+            for (int i = 0; i < MenuTitre.Length; i++)
+            {
+                Console.SetCursorPosition(Playground.SCREEN_WIDTH / 2 - 41, 2 + i);
+                Console.WriteLine(MenuTitre[i]);
+            }
+        }
+        public void DessinerMenuJouer()
+        {
+            for (int j = 0; j < MenuJouer.Length; j++)
+            {
+                Console.SetCursorPosition(Playground.SCREEN_WIDTH / 2 - 17, 12 + j);
+                Console.WriteLine(MenuJouer[j]);
+            }
+        }
+        public void DessinerMenuOption()
+        {
+            for (int k = 0; k < MenuOption.Length; k++)
+            {
+                Console.SetCursorPosition(Playground.SCREEN_WIDTH / 2 - 19, 18 + k);
+                Console.WriteLine(MenuOption[k]);
+            }
+        }
+        public void DessinerMenuScore()
+        {
+            for (int o = 0; o < MenuScore.Length; o++)
+            {
+                Console.SetCursorPosition(Playground.SCREEN_WIDTH / 2 - 16, 24 + o);
+                Console.WriteLine(MenuScore[o]);
+            }
+        }
+    }
+}
---
+++ Code/SpicyConso/Display/Playground.cs
@@ -0,0 +1,105 @@
+﻿using Model;
+
+namespace Display
+{
+    public class Playground
+    {
+        public const int SCREEN_HEIGHT = 40;
+        public const int SCREEN_WIDTH = 150;
+
+        public static string[] AlienSprit =
+        {
+            @" /-~-\",
+            @"|- # -|",
+            @" \―v―/ ",
+        };
+        public static string[] JoueurSprit =
+        {
+            @" _/-\_ ",
+            @"|#####|",
+        };
+        public static string Explosion = "";
+        public static string MissileJoueurSprit0 = "";
+        public static string MissileJoueurSprit1 = "‖";
+        public static string MissileJoueurSprit2 = "║";
+        public static string MissileJoueurSprit3 = "▲";
+        public static string MissileJoueurSprit4 = "ꜛ";
+        public static string MissileJoueurSprit5 = "◊";
+        public static void Initalisation()
+        {
+            Console.CursorVisible = false;
+            Console.SetWindowSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+            //Console.SetWindowSize(100, 100);
+        }
+
+        public static void DessinerAlien(Alien alien)
+        {
+            // modifer le boom par un ascii art d'explosion
+            if (!alien.AlienEstMort)
+            {
+                if (alien == null) return;
+                for (int i = 0; i < AlienSprit.Length; i++)
+                {
+                    Console.SetCursorPosition(alien.AlienX, alien.AlienY + i);
+                    Console.WriteLine(AlienSprit[i]);
+                }
+            }
+        }
+        public static void DessinerAlienMort(Alien alien)
+        {
+            if (alien.AlienEstMort)
+            {
+                for (int i = 0; i < AlienSprit.Length; i++)
+                {
+                    Console.SetCursorPosition(alien.AlienX, alien.AlienY + i);
+                    Console.WriteLine(Explosion[i]);
+                }
+            }
+        }
+
+        public static void DessinerJoueur(Joueur joueur)
+        {
+            for (int j = 0; j < JoueurSprit.Length; j++)
+            {
+                Console.SetCursorPosition(joueur.JoueurX, joueur.JoueurY + j);
+                Console.WriteLine(JoueurSprit[j]);
+            }
+        }
+        public static void DessinerJoueurMort(Joueur joueur)
+        {
+            if (joueur.JoueurEstMort)
+            {
+                for (int j = 0; j < JoueurSprit.Length; j++)
+                {
+                    Console.SetCursorPosition(joueur.JoueurX, joueur.JoueurY + j);
+                    Console.WriteLine(Explosion[j]);
+                }
+            }
+        }
+
+        public static void DessinerMissileDeclancher(MissileJoueur missile)
+        {
+            if (missile.MissileLancer)
+            {
+                if (missile == null)
+                {
+                    return;
+                }
+                Console.SetCursorPosition(missile.MissileX, missile.MissileY);
+                Console.Write(MissileJoueurSprit0);
+            }
+        }
+        public static void DessinerMissileAlien(MissileAlien missileAlien)
+        {
+            if (missileAlien.MissileLancer)
+            {
+                if (missileAlien == null)
+                {
+                    return;
+                }
+                Console.SetCursorPosition(missileAlien.MissileX + 3, missileAlien.MissileY);
+                Console.Write("█");
+            }
+        }
+    }
+}
