7275771999346eb29109e41020de6e4383f2d787
TheArabicMonster <neetamk@icloud.com>
2023-10-25 12:36:57 +0200

    ajout des frameRate + liaison MySQL et c#

--- Code/SpicyConso/SpicyConso/Program.cs
+++ Code/SpicyConso/SpicyConso/Program.cs
@@ -1,15 +1,21 @@
 ﻿using Display;
 using Model;
+using Storage;
 
+string jouPseudo = string.Empty;
 int ChoixMenu = 0;
 int CptAlien = 0;
 int ChanceAlienTirer = 0;
+int frameNumber = 0;
+int scoreJoueur = 0;
 bool ChoixJeux = true;
 bool GameOver = false;
 bool GameWin = false;
 bool Recommencer = true;
+bool quitter = false;
 ConsoleKeyInfo keyPressed;
 ConsoleKeyInfo response;
+ConsoleKeyInfo key;
 
 Playground.Initalisation();//Initialise la fenêtre de la console
 List<MissileAlien> MissileAlienASupprimer = new();//Liste pour stocker les missiles aliens à supprimer
@@ -24,6 +30,9 @@ Random random = new Random();
 
 do
 {
+    quitter = false;
+    jouPseudo = "";
+    scoreJoueur = 0;
     Recommencer = true;
     Joueur joueur = new(Console.WindowWidth / 2, Console.WindowHeight - 10);//Crée un joueur au centre de la fenêtre
     //création de 10 aliens
@@ -113,24 +122,29 @@ do
         ConsoleKeyInfo keyPressedGame;
         while (!GameOver && !GameWin)
         {
-            Console.Clear();
 
-            foreach (MissileJoueur missile in ListeMissilesJoueur)
+            if (frameNumber % 4 == 0)
             {
-                //dessine tout les missiles du joueur contenu dans ListeMissilesJoueur
-                Playground.DessinerMissileDeclancher(missile);
+                Console.Clear();
+
+                foreach (Alien alien in ListeAlien)
+                {
+                    //Dessine tout les aliens dans la liste ListeAlien
+                    Playground.DessinerAlien(alien);
+                }
+                Playground.DessinerJoueur(joueur);
+                foreach (MissileAlien missileAlien in ListeMissileAlien)
+                {
+                    //dessine tout les missiles aliens contenu dans ListeMissileAlien
+                    Playground.DessinerMissileAlien(missileAlien);
+                }
+                foreach (MissileJoueur missile in ListeMissilesJoueur)
+                {
+                    //dessine tout les missiles du joueur contenu dans ListeMissilesJoueur
+                    Playground.DessinerMissileDeclancher(missile);
+                }
             }
-            foreach (MissileAlien missileAlien in ListeMissileAlien)
-            {
-                //dessine tout les missiles aliens contenu dans ListeMissileAlien
-                Playground.DessinerMissileAlien(missileAlien);
-            }
-            foreach (Alien alien in ListeAlien)
-            {
-                //Dessine tout les aliens dans la liste ListeAlien
-                Playground.DessinerAlien(alien);
-            }
-            Playground.DessinerJoueur(joueur);
+            frameNumber++;
 
             if (Console.KeyAvailable)
             {
@@ -153,46 +167,53 @@ do
                 }
             }
 
-            foreach (MissileJoueur missile in ListeMissilesJoueur)
+            if (frameNumber % 1 == 0)
             {
-                missile.ActualiserMissile();
-
-                foreach (Alien alien in ListeAlien)
+                foreach (MissileJoueur missile in ListeMissilesJoueur)
                 {
-                    //si le missile du joueur touche un alien -> entre dans la boucle
-                    if (MissileAlien.CollisionMissileJoueurDansAlien(missile, alien))
+                    missile.ActualiserMissile();
+
+                    foreach (Alien alien in ListeAlien)
                     {
-                        //soustrais des HP de l'aliens égale au dégats du MissileJoueur
-                        alien.AlienHP -= missile.MissileDMG;
-                        //si les HP de l'alien sont = 0, il est considérer comme mort
-                        if (alien.AlienHP <= 0)
+                        //si le missile du joueur touche un alien -> entre dans la boucle
+                        if (MissileAlien.CollisionMissileJoueurDansAlien(missile, alien))
                         {
-                            alien.AlienEstMort = true;
+                            //soustrais des HP de l'aliens égale au dégats du MissileJoueur
+                            alien.AlienHP -= missile.MissileDMG;
+                            //si les HP de l'alien sont = 0, il est considérer comme mort
+                            if (alien.AlienHP <= 0)
+                            {
+                                alien.AlienEstMort = true;
+                                scoreJoueur += 25;
+                            }
+                            //ajout du missile qui a toucher l'alien dans la liste des missiles qui vont se faire supprimer
+                            MissilesASupprimer.Add(missile);
                         }
-                        //ajout du missile qui a toucher l'alien dans la liste des missiles qui vont se faire supprimer
-                        MissilesASupprimer.Add(missile);
                     }
                 }
             }
+
             //Supprime tout les aliens qui ont des hp égale à 0
             ListeAlien.RemoveAll(alien => alien.AlienHP <= 0); //chatgpt m'a aider avec ca
-
             foreach (MissileJoueur missile in MissilesASupprimer)
             {
                 //supprimer les missiles qui ont déjà toucher un alien
                 ListeMissilesJoueur.Remove(missile);
             }
 
-            foreach (MissileAlien missileAlien in ListeMissileAlien)
+            if (frameNumber % 2 == 0)
             {
-                missileAlien.MissileActualiseAlien(missileAlien);
-                //si le missile de l'alien touche le joueur il entre dans la boucle
-                if (joueur.CollisionMissileAlien(missileAlien))
+                foreach (MissileAlien missileAlien in ListeMissileAlien)
                 {
-                    //le missile de l'alien qui infliger des DMG au joueur
-                    joueur.PrendreDegats(missileAlien.MissileDMG);
-                    //ajout du missile alien à la liste des missiles aliens à supprimer
-                    MissileAlienASupprimer.Add(missileAlien);
+                    missileAlien.MissileActualiseAlien(missileAlien);
+                    //si le missile de l'alien touche le joueur il entre dans la boucle
+                    if (joueur.CollisionMissileAlien(missileAlien))
+                    {
+                        //le missile de l'alien qui infliger des DMG au joueur
+                        joueur.PrendreDegats(missileAlien.MissileDMG);
+                        //ajout du missile alien à la liste des missiles aliens à supprimer
+                        MissileAlienASupprimer.Add(missileAlien);
+                    }
                 }
             }
             //supprimer les missiles qui ont toucher le joueur
@@ -200,21 +221,31 @@ do
             {
                 ListeMissileAlien.Remove(missile);
             }
-
-            foreach (Alien alien in ListeAlien)
+            if (frameNumber % 2 == 0)
             {
-                //bouge les aliens de droite à gauche dans la console
-                alien.DeplacementDroiteAlien();
-                alien.DeplacementGaucheAlien();
-
-                ChanceAlienTirer = random.Next(1, 51); //génère un nombre entre 1 et 50
-                //si le nombre crée est 1, l'alien tire
-                if (ChanceAlienTirer == 1)
+                foreach (Alien alien in ListeAlien)
                 {
-                    MissileAlien nouveauMissilleAlien = new MissileAlien(alien);
-                    ListeMissileAlien.Add(nouveauMissilleAlien);
+                    //bouge les aliens de droite à gauche dans la console
+                    alien.DeplacementDroiteAlien();
+                    alien.DeplacementGaucheAlien();
                 }
             }
+
+            if (frameNumber % 5 == 0)
+            {
+                foreach (Alien alien in ListeAlien)
+                {
+                    ChanceAlienTirer = random.Next(1, 51); //génère un nombre entre 1 et 50
+                                                           //si le nombre crée est 1, l'alien tire
+                    if (ChanceAlienTirer == 1)
+                    {
+                        MissileAlien nouveauMissilleAlien = new MissileAlien(alien);
+                        ListeMissileAlien.Add(nouveauMissilleAlien);
+                    }
+                }
+            }
+
+
             //si la joueur a tuer tout les aliens, le jeux deviens gaganer
             if (ListeAlien.Count == 0)
             {
@@ -226,7 +257,7 @@ do
                 GameOver = true;
             }
 
-            Thread.Sleep(10); //timing
+            Thread.Sleep(3); //timing
             //si le jeux est gagner ou perdu, il entre dans la boucle
             if (GameOver || GameWin)
             {
@@ -238,14 +269,60 @@ do
                 {
                     menu.DessinerGameOver();
                 }
-                Console.SetCursorPosition(0, 0);
-                //Demande au joueur si il veux recommencer une partie ou quitter le programme
-                Console.WriteLine("Voulez-vous rejouer ? (Appuyez sur 'y' pour Oui, ou 'N' pour Non)");
+                Console.SetCursorPosition(0, 1);
+                Console.WriteLine("Appuyez sur echap pour passer ou entrer votre pseudo pour save votre score : ");
+
+                while (true)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        key = Console.ReadKey(true);
+                        if (key.Key == ConsoleKey.Escape)//Si echap est appuyer, passe la boucle
+                        {
+                            quitter = true;
+                        }
+                        else if (key.Key == ConsoleKey.Enter)//Si enter est appuyer, soit score est save, soit si le pseudo n'est pas valide affiche un message d'erreur
+                        {
+                            if (!string.IsNullOrEmpty(jouPseudo))
+                            {
+                                Store.SauvgarderScore(scoreJoueur, jouPseudo);
+                                quitter = false;
+                            }
+                            else
+                            {
+                                Console.SetCursorPosition(0, 2);
+                                Console.WriteLine("Le pseudo ne doit pas être null");
+                            }
+                        }
+                        else if (key.Key == ConsoleKey.Backspace)
+                        {
+                            //Retirer un caractère si Backspace est pressé
+                            if (jouPseudo.Length > 0)
+                            {
+                                jouPseudo = jouPseudo.Substring(0, jouPseudo.Length - 1);
+                                Console.SetCursorPosition(0, Console.CursorTop);
+                                Console.Write(new string(' ', Console.WindowWidth - 1));
+                                Console.SetCursorPosition(0, Console.CursorTop);
+                                Console.Write(jouPseudo);
+                            }
+                        }
+                        else
+                        {
+                            //Ajouter le caractère saisi au pseudo
+                            jouPseudo += key.KeyChar;
+                            Console.SetCursorPosition(20, 0);
+                            Console.Write(key.KeyChar);
+                        }
+                    }
+                    if (quitter) break;
+                }
             }
         }
-
         do
         {
+            Console.SetCursorPosition(0, 3);
+            //Demande au joueur si il veux recommencer une partie ou quitter le programme
+            Console.WriteLine("Voulez-vous rejouer ? (Appuyez sur 'y' pour Oui, ou 'N' pour Non)");
             response = Console.ReadKey();
             //si le joueur à appuyer sur "Y" il réinitialise les variable et clear les listes pour recommencer une partie
             if (response.Key == ConsoleKey.Y)
@@ -261,6 +338,8 @@ do
                 GameWin = false;
                 joueur.JoueurEstMort = false;
                 Recommencer = false;
+                quitter = false;
+                jouPseudo = string.Empty;
                 break;
             }
             //sinon, si il a appuyer sur "N" il quitte le programme
@@ -277,7 +356,20 @@ do
     } while (!ChoixJeux) ;
     if (ChoixMenu == 3)//afficher le menu score
     {
-
+        Store.Afficher5TopJoueurs();
+        Console.WriteLine("Appuyer sur échap pour revenir au menu principale");
+        while (true)//si utilisateur appuie sur echap, 
+        {
+            if (Console.KeyAvailable)
+            {
+                key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Escape)
+                {
+                    ChoixJeux = true;
+                    break; // Sort de la boucle lorsque Échap est pressé
+                }
+            }
+        }
     } while (!ChoixJeux) ;
 
 } while (true);
--- Code/SpicyConso/Storage/Store.cs
+++ Code/SpicyConso/Storage/Store.cs
@@ -1,18 +1,67 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Model;
+using MySqlConnector;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Model;
 
 namespace Storage
 {
     public class Store
     {
-        public static void StoreAlien (Alien alain)
+        //String pour se connecter au serveur 
+        public static string dbConnexion = "Server=localhost;Port=8888;Database=db_space_invaders;User=root;Password=root;";
+        //Commande MySQL pour savoir les top 5 meilleurs joueurs
+        public static string commandeSQLTopJoueur = "SELECT * FROM t_joueur ORDER BY jouNombrePoints DESC LIMIT 5;";
+        //Commande MySQL pour ajouter le score et le pseudo du joueur dans la base de données
+        public static string commandeSQLInsertScoreJoueur = "INSERT INTO t_joueur (jouNom, jouNombrePoints) VALUES (@jouPseudo, @scoreJoueur)";
+        public static void StoreAlien(Alien alain)
         {
-            Debug.WriteLine("C'est dans la db que je mets "+alain.ToString ());
+            Debug.WriteLine("C'est dans la db que je mets " + alain.ToString());
+        }
+        /// <summary>
+        /// affiche les 5 meilleurs joueurs de la base de données
+        /// </summary>
+        public static void Afficher5TopJoueurs()
+        {
+            using (MySqlConnection connexion = new MySqlConnection(dbConnexion))
+            {
+                connexion.Open();
+
+                using (MySqlCommand cmd = new MySqlCommand(commandeSQLTopJoueur, connexion))
+                {
+
+                    using (MySqlDataReader select = cmd.ExecuteReader())
+                    {
+                        while (select.Read())//Boucle pour afficher les meilleures joueurs 
+                        {
+                            int id = select.GetInt32("jouID");
+                            string nom = select.GetString("jouNom");
+                            int score = select.GetInt32("jouNombrePoints");
+                            Console.WriteLine($"ID: {id}, Nom: {nom}, Points: {score}");
+                        }
+                    }
+                }
+            }
+        }
+        /// <summary>
+        /// Méthode pour sauvgarder dans la base de données le score d'un joueur
+        /// </summary>
+        /// <param name="scoreJoueur">le score du joueur que l'ont vas sauvgarder</param>
+        /// <param name="jouPseudo">le pseudo du joueur que l'ont vas sauvgarder</param>
+        public static void SauvgarderScore(int scoreJoueur, string jouPseudo)
+        {
+            using (MySqlConnection connexion = new MySqlConnection(dbConnexion))
+            {
+                connexion.Open();//Se connecter à la base de données 
+
+                using (MySqlCommand cmd = new MySqlCommand(commandeSQLInsertScoreJoueur, connexion))
+                {
+                    cmd.Parameters.AddWithValue("@pseudo", jouPseudo);
+                    cmd.Parameters.AddWithValue("@score", scoreJoueur);
+                    cmd.ExecuteNonQuery();
+
+                    Console.SetCursorPosition(5, 0);
+                    Console.WriteLine($"{jouPseudo} à sauvergder un score de : {scoreJoueur}.");
+                }
+            }
         }
     }
 }
