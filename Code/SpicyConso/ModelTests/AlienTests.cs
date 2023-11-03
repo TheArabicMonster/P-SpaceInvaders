92163b2f430713c7ddc1f28a3c28a84f8ea0cdc1
TheArabicMonster <102302877+TheArabicMonster@users.noreply.github.com>
2023-11-03 09:09:16 +0100

    re ajout des test unitares
    arrivé à l'école mes test était introuvable, je les ai donc re ajouter

---
+++ Code/SpicyConso/ModelTests/AlienTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Model.Tests
+{
+    [TestClass()]
+    public class AlienTests
+    {
+        [TestMethod()]
+        public void AlienTest()
+        {
+            // Arrange
+            int X = 10;
+            int Y = 5;
+            int HP = 20;
+
+            // Act
+            Alien alien = new Alien(X, Y, HP);
+
+            // Assert
+            Assert.AreEqual(X, alien.AlienX);
+            Assert.AreEqual(Y, alien.AlienY);
+            Assert.AreEqual(HP, alien.AlienHP);
+            Assert.IsFalse(alien.AlienEstMort);
+        }
+
+        [TestMethod()]
+        public void DeplacementDroiteAlienTest()
+        {
+            // Arrange
+            Alien alien = new Alien(0, 0, 0);
+
+            // Act
+            alien.DeplacementDroiteAlien();
+
+            // Assert
+            Assert.AreEqual(2, alien.AlienX);
+        }
+
+        [TestMethod()]
+        public void DeplacementDroiteAlienTest_AlienChangeDirection()
+        {
+            // Arrange
+            int largeurEcran = 150;
+            int largeurAlien = 6;
+            Alien alien = new Alien(largeurEcran - largeurAlien - 2, 0, 0);
+
+            // Act
+            alien.DeplacementDroiteAlien();
+
+            // Assert
+            Assert.AreEqual(largeurEcran - largeurAlien, alien.AlienX);
+            Assert.IsFalse(alien.AlienDirection);
+        }
+    }
+}
---
+++ Code/SpicyConso/ModelTests/JoueurTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Model.Tests
+{
+    [TestClass()]
+    public class JoueurTests
+    {
+        [TestMethod()]
+        public void JoueurTest()
+        {
+            // Arrange
+            int X = 10;
+            int Y = 5;
+
+            // Act
+            Joueur joueur = new Joueur(X, Y);
+
+            // Assert
+            Assert.AreEqual(X, joueur.JoueurX);
+            Assert.AreEqual(Y, joueur.JoueurY);
+            Assert.IsFalse(joueur.JoueurEstMort);
+            Assert.AreEqual(20, joueur.JoueurHP);
+        }
+
+        [TestMethod()]
+        public void DeplacementJoueurDroiteTest()
+        {
+            // Arrange
+            Joueur joueur = new Joueur(0, 0);
+
+            // Act
+            joueur.DeplacementJoueurDroite();
+
+            // Assert
+            Assert.AreEqual(4, joueur.JoueurX);
+        }
+
+        [TestMethod()]
+        public void DeplacementJoueurDroiteTest_NeDépassePasLargeurEcran()
+        {
+            // Arrange
+            int largeurEcran = 150;
+            Joueur joueur = new Joueur(largeurEcran - 7, 0);
+
+            // Act
+            joueur.DeplacementJoueurDroite();
+
+            // Assert
+            Assert.AreEqual(largeurEcran - 7, joueur.JoueurX);
+        }
+    }
+}
